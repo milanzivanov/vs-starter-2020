df62c605cead2398d8a41abb18e2b8c14225a07d
milan.zivanov <milan.zivanov@gmail.com>
2020-07-16 23:20:01 +0200

    first commit

--- app/js/script.js
+++ app/js/script.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
     'use strict';
     
     console.log("WORKING");
+
+    // open menu
+    $(".nav-dropdown--js").on("click", function(e) {
+        $(".nav-dropdown__box").toggleClass("active");
+    });
 });
